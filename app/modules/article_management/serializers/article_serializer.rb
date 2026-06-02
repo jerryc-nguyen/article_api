3b2af0e5b987d55e72142c2792ff4f8652e885c4
Nhan Nguyen <jerryc.nguyen91@gmail.com>
2026-06-02 09:55:04 +0700

    fix: restore [] fallback for main_article_body and key_facts in serializer

--- app/modules/article_management/serializers/article_serializer.rb
+++ app/modules/article_management/serializers/article_serializer.rb
@@ -4,14 +4,16 @@ module ArticleManagement
       expose :title
 
       PARSED_FIELD_NAMES = %i[intro_hook main_article_body best_for not_for ethics_safety_notes key_facts].freeze
+      ARRAY_FIELDS = %i[main_article_body key_facts].freeze
 
       PARSED_FIELD_NAMES.each do |field|
         expose field do |article|
-          if article.updated_fields&.key?(field.to_s)
-            article.updated_fields[field.to_s]
-          else
-            article.parsed_fields&.dig(field.to_s)
-          end
+          value = if article.updated_fields&.key?(field.to_s)
+                    article.updated_fields[field.to_s]
+                  else
+                    article.parsed_fields&.dig(field.to_s)
+                  end
+          ARRAY_FIELDS.include?(field) ? value || [] : value
         end
       end
     end
